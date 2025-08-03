35c53ea9a5201f87e49874dd85538d808b5138db
Fer <lf0170885@gmail.com>
2025-08-03 02:17:08 +0200

    Actualizar admin con clientId y token correctos

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/Users/Fer/OneDrive/AAAAAAAAAAAAAAAAAAAA - copia/tina/__generated__/.cache/1754179821512', url: 'https://content.tinajs.io/1.6/content/83be6128-79a5-4658-8e2a-6f5e60de07e0/github/main', token: '662651dd02eaff2fb815d9fc480baebedd681067', queries,  });
+export const client = createClient({ cacheDir: 'C:/Users/Fer/OneDrive/AAAAAAAAAAAAAAAAAAAA - copia/tina/__generated__/.cache/1754180123428', url: 'https://content.tinajs.io/1.6/content/83be6128-79a5-4658-8e2a-6f5e60de07e0/github/main', token: '662651dd02eaff2fb815d9fc480baebedd681067', queries,  });
 export default client;
   
